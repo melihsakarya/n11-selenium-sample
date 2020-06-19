3ca3aed42ec9e315691a65ce8d8fe26ded8be935
Melih Sakarya <melih.sakarya@gmail.com>
2020-06-19 10:08:39 +0300

    first commit

---
+++ src/test/java/com/n11/sample/test/selenium/SampleTest.java
@@ -0,0 +1,31 @@
+package com.n11.sample.test.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class SampleTest {
+
+    @Test
+    public void ilkLinkeTiklama(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedrivermac");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.n11.com");
+
+
+        List<WebElement> webElemanlari = driver.findElements(By.tagName("a"));
+        System.out.println(webElemanlari.size());
+
+        WebElement ilkEleman = webElemanlari.get(0);
+        ilkEleman.click();
+
+
+
+        driver.quit();
+    }
+}
